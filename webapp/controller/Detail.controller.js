55970c8f9b9ddce773de8671da6693838115520b
Hugo Muschal <hugo.muschal@sulzer.de>
2024-12-09 12:51:58 +0000

    Detail view and controller added

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("studenthmu.com.sap.training.ux402.repository.ux402repository.controller.Detail", {
+        onInit() {
+        }
+    });
+});
